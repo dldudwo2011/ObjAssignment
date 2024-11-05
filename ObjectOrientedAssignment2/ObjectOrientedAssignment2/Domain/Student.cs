2b4c38ade0e7d4715cfbf9310ad6914833b18f7d
Youngjae Lee <ylee39@nait.ca>
2024-11-05 09:57:11 -0700

    commit

---
+++ ObjectOrientedAssignment2/ObjectOrientedAssignment2/Domain/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedAssignment2.Domain
+{
+    public class Student
+    {
+        private string _studentID = string.Empty; // Camel Case preceded with underscore
+        private string _firstName = "";
+
+        public string StudentID // Pascal case
+        {
+            get
+            {
+                return _studentID;
+            }
+            set
+            {
+                _studentID = value;
+            }
+        }
+
+        public string FirstName // Expression-Bodied Property Assessors
+        {
+            get => _firstName; // implementation can be made up of only a single statement
+            set => _firstName = value;
+        }
+
+        public string LastName { get; set; } = string.Empty; // Auto-Implemented Property
+        public string? Email { get; set; } // Nullable property
+
+        // Constructor with parameters to initialize StudentID, FirstName, LastName, and Email
+        public Student(string studentID, string firstName, string lastName, string? email)
+        {
+            _studentID = studentID;
+            _firstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+        // Default constructor if needed
+        public Student()
+        {
+            // Default constructor logic (if any)
+        }
+    }
+}
---
+++ ObjectOrientedAssignment2/ObjectOrientedAssignment2/main.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Data.SqlClient;
+using ObjectOrientedAssignment2.Domain;
+using System.Collections.Generic;
+
+namespace ObjectOrientedAssignment2
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BCS RequestDirector = new();
+
+            //bool Confirmation = RequestDirector.CreateProgram("", "CS123 course");
+            //Console.WriteLine(Confirmation);
+
+
+            //Student EnrolledStudent = RequestDirector.FindStudent("ylee39");
+            //Console.WriteLine("found student email: " + EnrolledStudent.Email);
+            //Console.WriteLine("found student FirstName: " + EnrolledStudent.FirstName);
+            //Console.WriteLine("found student LastName: " + EnrolledStudent.LastName);
+
+            //Student AcceptedStudent = new("newstudent12343w", "James", "lee", "skske1432@gmail.com");
+            //bool Confirmation = RequestDirector.EnrollStudent(AcceptedStudent, "CS123");
+            //Console.WriteLine(Confirmation);
+
+            //Student EnrolledStudent = RequestDirector.FindStudent("ylee39");
+            //Console.WriteLine("found student email: " + EnrolledStudent.Email);
+            //Console.WriteLine("found student FirstName: " + EnrolledStudent.FirstName);
+            //Console.WriteLine("found student LastName: " + EnrolledStudent.LastName);
+
+            //EnrolledStudent.Email = "skske1432@gmail.com";
+
+            //bool Confirmation = RequestDirector.ModifyStudent(EnrolledStudent);
+            //Console.WriteLine(Confirmation);
+
+            //Student EnrolledStudent = RequestDirector.FindStudent("ylee39");
+            //Console.WriteLine("found student email: " + EnrolledStudent.Email);
+            //Console.WriteLine("found student FirstName: " + EnrolledStudent.FirstName);
+            //Console.WriteLine("found student LastName: " + EnrolledStudent.LastName);
+
+
+
+            //bool Confirmation = RequestDirector.RemoveStudent(EnrolledStudent.StudentID);
+            //Console.WriteLine(Confirmation);
+
+            Domain.Program ActiveProgram = RequestDirector.FindProgram("CS101");
+            Console.WriteLine(ActiveProgram.ProgramCode);
+            Console.WriteLine("foundprogram description" + ActiveProgram.Description);
+
+            foreach (Student EnrolledStudent in ActiveProgram.EnrolledStudents)
+            {
+                Console.WriteLine("foundstudent-ID " + EnrolledStudent.StudentID);
+                Console.WriteLine("foundstudent-FirstName " + EnrolledStudent.FirstName);
+                Console.WriteLine("foundstudent-LastName " + EnrolledStudent.LastName);
+                Console.WriteLine("foundstudent-Email " + EnrolledStudent.Email);// Prints each item in the list
+            }
+        }
+    }
+}
